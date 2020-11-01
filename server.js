22c2398c3bcd38ca8a8f849f6d5dd60650138158
Bruno <m3rcurio.br@gmail.com>
2020-11-01 09:53:28 -0300

    body parser ordem

--- server.js
+++ server.js
@@ -11,18 +11,22 @@ const port = process.env.PORT;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 require('./server/src/controllers/index')(app);
 
 app.get('/ping', function (req, res) {
   return res.send('pong '+ process.env.REACT_APP_MY_SERVICE);
- });
+});
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));

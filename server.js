c0cdc1fad1779a6c495b4e176270ab03c0605d3e
Bruno <m3rcurio.br@gmail.com>
2020-11-01 13:29:13 -0300

    refactor https

--- server.js
+++ server.js
@@ -8,29 +8,31 @@ dotenv.config();
 const port = process.env.PORT || 3005;
 const app = express();
 
-
-
 app.use(favicon(__dirname + '/build/favicon.ico'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-if(process.env.NODE_ENV==="production")
-app.get('*',function(req,res,next){
-  if(req.headers['x-forwarded-proto'] != 'https'){
-    res.redirect(process.env.REACT_APP_MY_SERVICE+req.url);
-  } else next();
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+  return res.send('pong ' + process.env.REACT_APP_MY_SERVICE);
 });
 
 require('./server/src/controllers/index')(app);
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong '+ process.env.REACT_APP_MY_SERVICE);
-});
-app.get('', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-console.log("start server: "+ process.env.REACT_APP_MY_SERVICE)
+if (process.env.NODE_ENV === "production") {
+  app.get('*', function (req, res) {
+    if (req.headers['x-forwarded-proto'] != 'https')
+      res.redirect(process.env.REACT_APP_MY_SERVICE + req.url);
+    else res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+} else {
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
+
+console.log("start server: " + process.env.REACT_APP_MY_SERVICE)
 app.listen(port);

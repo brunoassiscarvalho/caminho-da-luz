53d2ba90ecc12d9d71693f60d8c6f956874c2c39
Bruno <m3rcurio.br@gmail.com>
2020-11-01 09:14:18 -0300

    favicon

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 const port = process.env.PORT || 3005;
+
 const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
